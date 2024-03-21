11c2698b1ce41185fa015107ae66f0613acba6ce
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2024-03-21 21:17:21 +0900

    add error code

--- src/api/axios.instance.ts
+++ src/api/axios.instance.ts
@@ -18,19 +18,29 @@ axiosInstance.interceptors.response.use(
 		return response.data;
 	},
 	async (error) => {
-		const message = error.response.data.message;
+		console.log(error);
+
+		const message = error.response?.data?.message;
+
+		const error_code = error.response?.data?.code;
 
 		if (!message) {
 			return alert('잘못된 접근');
 		}
 
-		// const error_code = error.response.data.code;
+		if (!error_code) {
+			return alert('잘못된 접근');
+		}
 
 		// const kakao_error_code = error.response.data.error_code;
 
 		// if (kakao_error_code.include('KOE')) {
 		// 	return alert('잘못된 접근입니다. 다시 로그인하세요!');
 		// }
+
+		if (error_code === 1000) {
+			return alert('잘못된 접근입니다. 다시 로그인하세요!');
+		}
 
 		return alert(`${message}`);
 	}
