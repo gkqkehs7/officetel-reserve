ed12634a8236434b6a43608da9a8cc32c9756a76
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2024-03-21 21:01:41 +0900

    custom error

--- src/api/axios.instance.ts
+++ src/api/axios.instance.ts
@@ -13,4 +13,27 @@ axiosInstance.interceptors.request.use(
 	}
 );
 
+axiosInstance.interceptors.response.use(
+	(response) => {
+		return response.data;
+	},
+	async (error) => {
+		const message = error.response.data.message;
+
+		if (!message) {
+			return alert('잘못된 접근');
+		}
+
+		// const error_code = error.response.data.code;
+
+		// const kakao_error_code = error.response.data.error_code;
+
+		// if (kakao_error_code.include('KOE')) {
+		// 	return alert('잘못된 접근입니다. 다시 로그인하세요!');
+		// }
+
+		return alert(`${message}`);
+	}
+);
+
 export default axiosInstance;
